8ec3cd4bbd1a0395b739824ac599bde73161d6e1
deivezera <dzabroski@gmail.com>
2026-07-22 09:56:11 -0300

    feat: places page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Output the copyright year dynamically
   const currentYearElement = document.getElementById("currentyear");
   if (currentYearElement) {
     currentYearElement.innerHTML = new Date().getFullYear();
   }
 
-  // Output document last modified dynamically
   const lastModifiedElement = document.getElementById("lastModified");
   if (lastModifiedElement) {
     lastModifiedElement.innerHTML = `Last Modification: ${document.lastModified}`;
